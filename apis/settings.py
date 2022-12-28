24e0647a0393895b58ffe7733a54dc29733581b7
ashok <neupane.ashok.9696@gmail.com>
2022-12-29 01:23:16 +0545

    social login in Progress

--- apis/settings.py
+++ apis/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dictionary'
+    'dictionary',
+    'social_django'#added
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware'#added
 ]
 
 ROOT_URLCONF = 'apis.urls'
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',#added
             ],
         },
     },
@@ -119,3 +122,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#added everything below this
+AUTHENTICATION_BACKENDS = [
+    'social_core.backends.google.GoogleOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+]
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_URL = 'logout'
+LOGOUT_REDIRECT_URL = 'login'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '##################################'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '###########################'
--- dictionary/views.py
+++ dictionary/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"dictionary/index.html")
+
+
+def login(request):
+    return render(request,"dictionary/login.html")
